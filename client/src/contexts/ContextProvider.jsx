40b65304caf5cec7aa9127523dccad72f07d9132
worldwarmworm <st.valeriq@gmail.com>
2024-01-17 21:03:04 +0700

    protect layout from guests, add styles into src/index.css

--- client/src/contexts/ContextProvider.jsx
+++ client/src/contexts/ContextProvider.jsx
@@ -1,3 +1,34 @@
-import {createContext} from "react";
+import { createContext, useContext, useState } from "react"
 
-const StateContext = createContext()
+const StateContext = createContext({
+    user: null,
+    token: null,
+    setUser: () => {},
+    setToken: () => {},
+})
+
+export const ContextProvider = ({children}) => {
+    const accessToken = 'ACCESS_TOKEN'
+    const [user, setUser] = useState({})
+    const [token, _setToken] = useState(localStorage.getItem(accessToken))
+
+    const setToken = (token) => {
+        _setToken(token)
+        token
+            ? localStorage.setItem(accessToken, token)
+            : localStorage.removeItem(accessToken)
+    }
+
+    return (
+        <StateContext.Provider value={{
+            user,
+            token,
+            setUser,
+            setToken
+        }}>
+            {children}
+        </StateContext.Provider>
+    )
+}
+
+export const useStateContext = () => useContext(StateContext)
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import router from "../router.jsx";
 import './index.css'
 import {RouterProvider} from "react-router-dom";
+import { ContextProvider } from "./contexts/ContextProvider.jsx"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-      <RouterProvider router={router}>
-          <App />
-      </RouterProvider>
+      <ContextProvider>
+          <RouterProvider router={router} />
+      </ContextProvider>
   </React.StrictMode>,
 )
